a6ec0d36b4193db234b15b51dc41e6c78d2d15c5
Alb33rt <albert931023@gmail.com>
2021-10-13 01:24:56 +0800

    final version for homepage

--- test.js
+++
@@ -1,7 +0,0 @@
- const menuToggle = document.querySelector('.toggle');
- const mainpage = document.querySelector('.mainpage');
-
- menuToggle.addEventListener('click', () => {
-     menuToggle.classList.toggle('active');
-     mainpage.classList.toggle('active');
- })
