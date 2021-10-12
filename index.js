546de7af29c158652ab546d53a7a1fb7a316b740
Alb33rt <albert931023@gmail.com>
2021-10-13 01:11:31 +0800

    fixed navbar dropdown solution

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
- const menuToggle = document.querySelector('.toggle');
-      const mainpage = document.querySelector('.mainpage');
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+const mainpage = document.querySelector('.mainpage');
 
-      menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        mainpage.classList.toggle('active');
-      })
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+  mainpage.classList.toggle('active');
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
